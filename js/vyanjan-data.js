f5b04b949fb7366c6b02120dd7a5c38d50b1d364
biku-cyber <bikashshill662@gmail.com>
2026-06-26 00:08:47 +0530

    Initialize vyanjanData with letter objects

--- js/vyanjan-data.js
+++ js/vyanjan-data.js
@@ -1,20 +1,85 @@
-const vyanjanData = [];
+const vyanjanData = [
 
-for(let i=1;i<=41;i++){
+{id:1,letter:"ক",pronunciation:"Ka",word:"কপৌ",english:"Foxtail Orchid",image:"assets/vyanjan/vyanjan01.png",audio:"assets/audio/vyanjan/vyanjan01.mp3",narration:"assets/audio/vyanjan/vyanjan01-narration.mp3"},
 
-let num =
-String(i).padStart(2,"0");
+{id:2,letter:"খ",pronunciation:"Kha",word:"খৰম",english:"kharam",image:"assets/vyanjan/vyanjan02.png",audio:"assets/audio/vyanjan/vyanjan02.mp3",narration:"assets/audio/vyanjan/vyanjan02-narration.mp3"},
 
-vyanjanData.push({
+{id:3,letter:"গ",pronunciation:"Ga",word:"গৰু",english:"Cow",image:"assets/vyanjan/vyanjan03.png",audio:"assets/audio/vyanjan/vyanjan03.mp3",narration:"assets/audio/vyanjan/vyanjan03-narration.mp3"},
 
-letter:"",
+{id:4,letter:"ঘ",pronunciation:"Gha",word:"ঘৰ",english:"House",image:"assets/vyanjan/vyanjan04.png",audio:"assets/audio/vyanjan/vyanjan04.mp3",narration:"assets/audio/vyanjan/vyanjan04-narration.mp3"},
 
-pronunciation:"",
+{id:5,letter:"ঙ",pronunciation:"Nga",word:"নাঙল",english:"plough",image:"assets/vyanjan/vyanjan05.png",audio:"assets/audio/vyanjan/vyanjan05.mp3",narration:"assets/audio/vyanjan/vyanjan05-narration.mp3"},
 
-image:`assets/vyanjan/vyanjan${num}.png`,
+{id:6,letter:"চ",pronunciation:"Cha",word:"চৰাই",english:"Bird",image:"assets/vyanjan/vyanjan06.png",audio:"assets/audio/vyanjan/vyanjan06.mp3",narration:"assets/audio/vyanjan/vyanjan06-narration.mp3"},
 
-audio:`assets/audio/vyanjan/vyanjan${num}.mp3`
+{id:7,letter:"ছ",pronunciation:"Chha",word:"ছাগলী",english:"Goat",image:"assets/vyanjan/vyanjan07.png",audio:"assets/audio/vyanjan/vyanjan07.mp3",narration:"assets/audio/vyanjan/vyanjan07-narration.mp3"},
 
-});
+{id:8,letter:"জ",pronunciation:"Ja",word:"জাহাজ",english:"Ship",image:"assets/vyanjan/vyanjan08.png",audio:"assets/audio/vyanjan/vyanjan08.mp3",narration:"assets/audio/vyanjan/vyanjan08-narration.mp3"},
 
-}
+{id:9,letter:"ঝ",pronunciation:"Jha",word:"ঝিনুক",english:"Shell",image:"assets/vyanjan/vyanjan09.png",audio:"assets/audio/vyanjan/vyanjan09.mp3",narration:"assets/audio/vyanjan/vyanjan09-narration.mp3"},
+
+{id:10,letter:"ঞ",pronunciation:"Nya",word:"চিঞৰ",english:"scream",image:"assets/vyanjan/vyanjan10.png",audio:"assets/audio/vyanjan/vyanjan10.mp3",narration:"assets/audio/vyanjan/vyanjan10-narration.mp3"},
+
+{id:11,letter:"ট",pronunciation:"Ta",word:"টকা",english:"currency",image:"assets/vyanjan/vyanjan11.png",audio:"assets/audio/vyanjan/vyanjan11.mp3",narration:"assets/audio/vyanjan/vyanjan11-narration.mp3"},
+
+{id:12,letter:"ঠ",pronunciation:"Tha",word:"ঠেলা",english:"Cart",image:"assets/vyanjan/vyanjan12.png",audio:"assets/audio/vyanjan/vyanjan12.mp3",narration:"assets/audio/vyanjan/vyanjan12-narration.mp3"},
+
+{id:13,letter:"ড",pronunciation:"Da",word:"ডালিম",english:"Pomegranate",image:"assets/vyanjan/vyanjan13.png",audio:"assets/audio/vyanjan/vyanjan13.mp3",narration:"assets/audio/vyanjan/vyanjan13-narration.mp3"},
+
+{id:14,letter:"ঢ",pronunciation:"Dha",word:"ঢোল",english:"Drum",image:"assets/vyanjan/vyanjan14.png",audio:"assets/audio/vyanjan/vyanjan14.mp3",narration:"assets/audio/vyanjan/vyanjan14-narration.mp3"},
+
+{id:15,letter:"ণ",pronunciation:"Na",word:"কাণ",english:"Ear",image:"assets/vyanjan/vyanjan15.png",audio:"assets/audio/vyanjan/vyanjan15.mp3",narration:"assets/audio/vyanjan/vyanjan15-narration.mp3"},
+
+{id:16,letter:"ত",pronunciation:"Ta",word:"তৰা",english:"Star",image:"assets/vyanjan/vyanjan16.png",audio:"assets/audio/vyanjan/vyanjan16.mp3",narration:"assets/audio/vyanjan/vyanjan16-narration.mp3"},
+
+{id:17,letter:"থ",pronunciation:"Tha",word:"থালা",english:"Plate",image:"assets/vyanjan/vyanjan17.png",audio:"assets/audio/vyanjan/vyanjan17.mp3",narration:"assets/audio/vyanjan/vyanjan17-narration.mp3"},
+
+{id:18,letter:"দ",pronunciation:"Da",word:"দৰ্জা",english:"Door",image:"assets/vyanjan/vyanjan18.png",audio:"assets/audio/vyanjan/vyanjan18.mp3",narration:"assets/audio/vyanjan/vyanjan18-narration.mp3"},
+
+{id:19,letter:"ধ",pronunciation:"Dha",word:"ধনু",english:"Bow",image:"assets/vyanjan/vyanjan19.png",audio:"assets/audio/vyanjan/vyanjan19.mp3",narration:"assets/audio/vyanjan/vyanjan19-narration.mp3"},
+
+{id:20,letter:"ন",pronunciation:"Na",word:"নাও",english:"Boat",image:"assets/vyanjan/vyanjan20.png",audio:"assets/audio/vyanjan/vyanjan20.mp3",narration:"assets/audio/vyanjan/vyanjan20-narration.mp3"},
+
+{id:21,letter:"প",pronunciation:"Pa",word:"পখিলা",english:"Butterfly",image:"assets/vyanjan/vyanjan21.png",audio:"assets/audio/vyanjan/vyanjan21.mp3",narration:"assets/audio/vyanjan/vyanjan21-narration.mp3"},
+
+{id:22,letter:"ফ",pronunciation:"Pha",word:"ফুল",english:"Flower",image:"assets/vyanjan/vyanjan22.png",audio:"assets/audio/vyanjan/vyanjan22.mp3",narration:"assets/audio/vyanjan/vyanjan22-narration.mp3"},
+
+{id:23,letter:"ব",pronunciation:"Ba",word:"বল",english:"Ball",image:"assets/vyanjan/vyanjan23.png",audio:"assets/audio/vyanjan/vyanjan23.mp3",narration:"assets/audio/vyanjan/vyanjan23-narration.mp3"},
+
+{id:24,letter:"ভ",pronunciation:"Bha",word:"ভালুক",english:"Bear",image:"assets/vyanjan/vyanjan24.png",audio:"assets/audio/vyanjan/vyanjan24.mp3",narration:"assets/audio/vyanjan/vyanjan24-narration.mp3"},
+
+{id:25,letter:"ম",pronunciation:"Ma",word:"মাছ",english:"Fish",image:"assets/vyanjan/vyanjan25.png",audio:"assets/audio/vyanjan/vyanjan25.mp3",narration:"assets/audio/vyanjan/vyanjan25-narration.mp3"},
+
+{id:26,letter:"য",pronunciation:"Ya",word:"যতন",english:"Care",image:"assets/vyanjan/vyanjan26.png",audio:"assets/audio/vyanjan/vyanjan26.mp3",narration:"assets/audio/vyanjan/vyanjan26-narration.mp3"},
+
+{id:27,letter:"ৰ",pronunciation:"Ra",word:"ৰথ",english:"Chariot",image:"assets/vyanjan/vyanjan27.png",audio:"assets/audio/vyanjan/vyanjan27.mp3",narration:"assets/audio/vyanjan/vyanjan27-narration.mp3"},
+
+{id:28,letter:"ল",pronunciation:"La",word:"লাউ",english:"Bottle Gourd",image:"assets/vyanjan/vyanjan28.png",audio:"assets/audio/vyanjan/vyanjan28.mp3",narration:"assets/audio/vyanjan/vyanjan28-narration.mp3"},
+
+{id:29,letter:"ৱ",pronunciation:"Wa",word:"ৱৰষুণ",english:"Rain",image:"assets/vyanjan/vyanjan29.png",audio:"assets/audio/vyanjan/vyanjan29.mp3",narration:"assets/audio/vyanjan/vyanjan29-narration.mp3"},
+
+{id:30,letter:"শ",pronunciation:"Sha",word:"শিয়াল",english:"Fox",image:"assets/vyanjan/vyanjan30.png",audio:"assets/audio/vyanjan/vyanjan30.mp3",narration:"assets/audio/vyanjan/vyanjan30-narration.mp3"},
+
+{id:31,letter:"ষ",pronunciation:"Sha",word:"ষাঁড়",english:"Bull",image:"assets/vyanjan/vyanjan31.png",audio:"assets/audio/vyanjan/vyanjan31.mp3",narration:"assets/audio/vyanjan/vyanjan31-narration.mp3"},
+
+{id:32,letter:"স",pronunciation:"Sa",word:"সাপ",english:"Snake",image:"assets/vyanjan/vyanjan32.png",audio:"assets/audio/vyanjan/vyanjan32.mp3",narration:"assets/audio/vyanjan/vyanjan32-narration.mp3"},
+
+{id:33,letter:"হ",pronunciation:"Ha",word:"হাতী",english:"Elephant",image:"assets/vyanjan/vyanjan33.png",audio:"assets/audio/vyanjan/vyanjan33.mp3",narration:"assets/audio/vyanjan/vyanjan33-narration.mp3"},
+
+{id:34,letter:"ক্ষ",pronunciation:"Khya",word:"ক্ষীৰ",english:"Milk",image:"assets/vyanjan/vyanjan34.png",audio:"assets/audio/vyanjan/vyanjan34.mp3",narration:"assets/audio/vyanjan/vyanjan34-narration.mp3"},
+
+{id:35,letter:"ড়",pronunciation:"Ra",word:"বড়",english:"Big",image:"assets/vyanjan/vyanjan35.png",audio:"assets/audio/vyanjan/vyanjan35.mp3",narration:"assets/audio/vyanjan/vyanjan35-narration.mp3"},
+
+{id:36,letter:"ঢ়",pronunciation:"Rha",word:"আঢ়ৈ",english:"Two and Half",image:"assets/vyanjan/vyanjan36.png",audio:"assets/audio/vyanjan/vyanjan36.mp3",narration:"assets/audio/vyanjan/vyanjan36-narration.mp3"},
+
+{id:37,letter:"য়",pronunciation:"Yo",word:"য়ুৰোপ",english:"Europe",image:"assets/vyanjan/vyanjan37.png",audio:"assets/audio/vyanjan/vyanjan37.mp3",narration:"assets/audio/vyanjan/vyanjan37-narration.mp3"},
+
+{id:38,letter:"ৎ",pronunciation:"T",word:"সৎ",english:"Honest",image:"assets/vyanjan/vyanjan38.png",audio:"assets/audio/vyanjan/vyanjan38.mp3",narration:"assets/audio/vyanjan/vyanjan38-narration.mp3"},
+
+{id:39,letter:"ং",pronunciation:"Anuswar",word:"অংক",english:"Number",image:"assets/vyanjan/vyanjan39.png",audio:"assets/audio/vyanjan/vyanjan39.mp3",narration:"assets/audio/vyanjan/vyanjan39-narration.mp3"},
+
+{id:40,letter:"ঃ",pronunciation:"Bisarga",word:"দুঃখ",english:"Sorrow",image:"assets/vyanjan/vyanjan40.png",audio:"assets/audio/vyanjan/vyanjan40.mp3",narration:"assets/audio/vyanjan/vyanjan40-narration.mp3"},
+
+{id:41,letter:"ঁ",pronunciation:"Chandrabindu",word:"দাঁত",english:"tooth",image:"assets/vyanjan/vyanjan41.png",audio:"assets/audio/vyanjan/vyanjan41.mp3",narration:"assets/audio/vyanjan/vyanjan41-narration.mp3"}
+
+];
