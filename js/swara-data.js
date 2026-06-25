b9735585aab316b128c54c7c22b0a789aee2a88e
biku-cyber <bikashshill662@gmail.com>
2026-06-25 22:22:31 +0530

    Create swara-data.js

---
+++ js/swara-data.js
@@ -0,0 +1,25 @@
+const swaraData = [
+
+{letter:"অ",pronunciation:"A",image:"assets/swara/swara01.png",audio:"assets/audio/swara/swara01.mp3"},
+
+{letter:"আ",pronunciation:"AA",image:"assets/swara/swara02.png",audio:"assets/audio/swara/swara02.mp3"},
+
+{letter:"ই",pronunciation:"I",image:"assets/swara/swara03.png",audio:"assets/audio/swara/swara03.mp3"},
+
+{letter:"ঈ",pronunciation:"EE",image:"assets/swara/swara04.png",audio:"assets/audio/swara/swara04.mp3"},
+
+{letter:"উ",pronunciation:"U",image:"assets/swara/swara05.png",audio:"assets/audio/swara/swara05.mp3"},
+
+{letter:"ঊ",pronunciation:"UU",image:"assets/swara/swara06.png",audio:"assets/audio/swara/swara06.mp3"},
+
+{letter:"ঋ",pronunciation:"RI",image:"assets/swara/swara07.png",audio:"assets/audio/swara/swara07.mp3"},
+
+{letter:"এ",pronunciation:"E",image:"assets/swara/swara08.png",audio:"assets/audio/swara/swara08.mp3"},
+
+{letter:"ঐ",pronunciation:"OI",image:"assets/audio/swara/swara09.png",audio:"assets/audio/swara/swara09.mp3"},
+
+{letter:"ও",pronunciation:"O",image:"assets/swara/swara10.png",audio:"assets/audio/swara/swara10.mp3"},
+
+{letter:"ঔ",pronunciation:"OU",image:"assets/swara/swara11.png",audio:"assets/audio/swara/swara11.mp3"}
+
+];
