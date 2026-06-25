1baea1f0839a458ae79fe0decf3e1664fed5c9a0
biku-cyber <bikashshill662@gmail.com>
2026-06-25 22:11:14 +0530

    Add loader functionality to hide loader after 3 seconds

---
+++ js/loader.js
@@ -0,0 +1,15 @@
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+document
+.getElementById("loader")
+.style.display="none";
+
+document
+.getElementById("homePage")
+.classList.remove("hidden");
+
+},3000);
+
+});
